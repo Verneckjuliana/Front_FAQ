70e0ec98426eb22494fc14d698d1299a647de8e6
Verneckjuliana <verneckjuliana1@gmail.com>
2024-03-19 16:41:24 -0300

    tela1

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./styles.css";
+import Faixa from "./img/Barra1.jpg";
+import Logo from "./img/logo.png";
+
+function App() {
+  return (
+    <div>
+      <div className="navbar">
+        <img className="faixa" src={Faixa} alt="faixa bosch" />
+        <img className="logo" src={Logo} alt="logo bosch" />
+      </div>
+
+      <div>
+        <h1 className="title">Divisiones</h1>
+        <section className="box">
+          <div className="box1">
+            <div>
+              <h1 className="title2">HC</h1>
+              <h2 className="subtitle">Home Comfort</h2>
+            </div>
+          </div>
+          <div className="box1">
+            <div>
+              <h1 className="title2">PT</h1>
+              <h2 className="subtitle">Power Tools</h2>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className="footer">
+        <p className="footer-text">
+          © 2024 Robert Bosch. Todos los derechos reservados.
+        </p>
+        <img className="faixa2" src={Faixa} alt="faixa bosch" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
